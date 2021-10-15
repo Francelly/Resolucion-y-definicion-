c9ee53e759ff6f3ae56f853473172b86c52a1c90
Francelly <91504257+Francelly@users.noreply.github.com>
2021-10-14 19:26:59 -0500

    Add files via upload

---
+++ El_Libro.m
@@ -0,0 +1,11 @@
+% 
+% Title                :El libro
+% Description          :Script para definir y solucionar variable, funcion, dominio y rango
+% Authors              :Atzimba Joice Martinez Huitron
+%                      :Ana Cristina Franco Gonzales
+%                      :Francelly Desiree Colin Sandoval
+% Date                 :20211014
+% Version              :1
+% Usage                :octave> /path/El_Libro
+
+
